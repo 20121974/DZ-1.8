c1ef380bdfd1c17e59fb24ab84ffea35b6b81224
20121974 <nfi46102@mail.ru>
2022-08-29 11:53:24 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
+import java.time.LocalDate;
+
 public class Main {
 
-    public static void printSeparator() {
-        System.out.println("+++++++++++");
-        System.out.println("-----------");
-    }
-
-    public static void printIssues (int issueCount){
-        System.out.println(issueCount);
-    }
-
-    public static int sum (int [] numbers){//выполняет возврат наружу (посчитать внутри метода и возвратить наружу), вмето void берем тип и имя переменной
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }            
-        return sum;
-    }
     public static void main(String[] args) {
-        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8,};
-        printSeparator();
-        for (int i = 0; i < issuesByMonths.length; i++) {
-            printIssues(issuesByMonths[i]);
-            if ((i + 1) % 3 == 0) {
-                printSeparator();
-            }
+        printIsLeap(2022);
+        printInstallationMassage(1,2022);
+        printCalculateDeliveryDays (50);
+
+    }
+    public static void printIsLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Високосный год");
+        } else {
+            System.out.println("Не високосный год");
         }
-        printSeparator();
-        int total = sum(issuesByMonths);
-        printIssues(total);
+    }
+    public static void printInstallationMassage(int os, int productionYear) {
+        if (!(os == 0 || os == 1)) {
+            throw new RuntimeException("устройство не поддерживается");}
+
+        var currentYear = LocalDate.now().getYear();//Это приведет к запросу системных часов в часовом поясе по умолчанию для получения текущей даты.
+
+        String versionMassage = productionYear < currentYear ? "облегченную": " ";
+        String osMassage = os == 0 ? "iOS" : "android";
+        System.out.printf("Установите %s версию приложения для %s по ссылке", versionMassage, osMassage);
+        System.out.println("");
+    }
+    public static void printCalculateDeliveryDays(int distance) {
+    int start = 20;
+    int step = 40; //шаг
+    int result = 1;
+    if (distance >= start) {
+        result = (Math.abs((distance - start) / step) + 2);}
+        System.out.println("Потребуется для доставки дней " + result);
     }
 }
+
+
