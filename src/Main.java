cf7189a72024be59a63d3971f3b858563ce4b24b
20121974 <nfi46102@mail.ru>
2022-08-31 10:08:34 +0300

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         printIsLeap(2022);
-        printInstallationMassage(1,2022);
         printCalculateDeliveryDays (50);
 
     }
@@ -15,23 +14,26 @@ public class Main {
             System.out.println("Не високосный год");
         }
     }
-    public static void printInstallationMassage(int os, int productionYear) {
-        if (!(os == 0 || os == 1)) {
+    public static void printInstallationMessage(int os, int productionYear) {//Метод печати сообщения об установке
+       if (!(os == 0 || os == 1)) {
             throw new RuntimeException("устройство не поддерживается");}
+    choosingVersion (0, 2015);
+    }
 
+    public static void choosingVersion (int os, int productionYear){
         var currentYear = LocalDate.now().getYear();//Это приведет к запросу системных часов в часовом поясе по умолчанию для получения текущей даты.
 
         String versionMassage = productionYear < currentYear ? "облегченную": " ";
         String osMassage = os == 0 ? "iOS" : "android";
         System.out.printf("Установите %s версию приложения для %s по ссылке", versionMassage, osMassage);
-
     }
     public static void printCalculateDeliveryDays(int distance) {
     int start = 20;
     int step = 40; //шаг
     int result = 1;
     if (distance >= start) {
-        result = (Math.abs((distance - start) / step) + 2);}
+        result = (Math.abs((distance - start) / step) + 2);
+    }
         System.out.println("");
     System.out.println("Потребуется для доставки дней " + result);
     }
